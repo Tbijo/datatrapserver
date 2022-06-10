65d297a673da6f00401771d4d5f99c39685656ad
Tibor Fábry <tibor.j.fabry@gmail.com>
2022-06-10 22:11:09 +0200

    Skoro hotovy ImageController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\imagesync\ImageSync;
+use App\Models\MouseImage;
+use App\Models\OccasionImage;
+use App\Models\SpecieImage;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
     public function insert(Request $request) {
-        return response("INSERT", 200);
+        $data = $request->all();
+        $imageSync = new ImageSync($data);
+
+        foreach($imageSync->mouseImages as $key => $value) {
+            $mouseImage = new MouseImage($value);
+            MouseImage::create(json_decode($mouseImage, true));
+        }
+
+        foreach($imageSync->occasionImages as $key => $value) {
+            $occasionImage = new OccasionImage($value);
+            OccasionImage::create(json_decode($occasionImage, true));
+        }
+
+        foreach($imageSync->specieImages as $key => $value) {
+            $specieImage = new SpecieImage($value);
+            SpecieImage::create(json_decode($specieImage, true));
+        }
+
+        if ($request->hasFile("fileImages")) {
+            foreach($imageSync->fileImages as $fileImage) {
+                // storing files
+            }
+        }
+
+        return response("Funguje", 200);
     }
 }
